6953bb1c35563c7a4dbb0b1a0c14fe674d21ac47
adityaparab04 <adityaparab04@gmail.com>
2021-05-13 16:41:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+ //this is my first java script code
+ let name = 'Aditya Parab';
+ console.log(name);
+ let firstName = 'Aditya', lastName ='Parab' ;
+ console.log(firstName,lastName);
+ var x = 2;
+ x = 3;
+ console.log(x);
+
+ let isApproved = true;
+ let middleName = undefined;
+ let selectedColor = null;
