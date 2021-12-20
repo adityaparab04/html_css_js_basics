f92198518120842930743284cdcbe04e521d7cb7
adityaparab04 <adityaparab04@gmail.com>
2021-12-19 23:16:42 -0500

    pass by reference and value

---
+++ pass_by.js
@@ -0,0 +1,26 @@
+// let age = 22;
+// let name = 'Aditya Parab';
+// let grades = ['A', 'C+', 'A+'];
+
+// let d = grades;
+// let e = age;
+// e= e+1;
+// console.log(e)
+// console.log(age)
+// d = [1, 2, 3]
+
+// grades.push('A');
+// console.log(grades);
+// console.log(d);
+
+
+let x = [10, 12];
+console.log(x);
+
+add(x, 6);
+console.log(x);
+
+function add(arr, ele){
+    arr = [ele];
+    console.log(arr)
+}
